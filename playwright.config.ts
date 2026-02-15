dc62fdefb7db3e5687c88af9940a687003225d06
Bernardo Duarte <bernardoduarte.mg@gmail.com>
2026-02-15 18:11:25 -0300

    fix: configure webServer and firefox env

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,6 +13,14 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
+
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:5173',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000, // Dá 2 minutos para o servidor subir se for lento
+  },
+
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -25,10 +33,7 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    baseURL: 'http://localhost:5173', // Garante que o teste use essa URL base
     trace: 'on-first-retry',
   },
 
